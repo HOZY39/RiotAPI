e083ca4ee6d084e3bdacbe6fc695bd06f2c7a8b8
Igor Simon <75307125+HOZY39@users.noreply.github.com>
2024-06-26 16:14:21 +0200

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -373,11 +373,10 @@ class Missions:
         self.playerScore9 = playerScore9
 
 class Summoner:
-  def __init__(self, id, accountId, puuid, name, profileIconId, revisionDate, summonerLevel):
+  def __init__(self, id, accountId, puuid, profileIconId, revisionDate, summonerLevel):
     self.id = id
     self.accountId = accountId
     self.puuid = puuid
-    self.name = name
     self.profileIconId = profileIconId
     self.revisionDate = revisionDate
     self.summonerLevel = summonerLevel
